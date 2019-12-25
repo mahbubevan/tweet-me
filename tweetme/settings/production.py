55b3da0e3453dad64ca1fb147fc6389560f55d6d
mahbubevan <evanpulse92@gmail.com>
2019-12-25 10:11:00 +0600

    Created Base,Local,Production Setting

--- tweetme/settings/production.py
+++ tweetme/settings/production.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'bh46b_2md@yqqww*dg0#@-^7e414ukdst4#n9^^924q24legtj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
